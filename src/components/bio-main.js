b2f4f57ab872c9d37fc51bb316d840f1346ce99d
LupenTiJay <jayant.tiwari777@gmail.com>
2019-09-02 23:10:38 -0400

    working landing state

--- src/components/bio-main.js
+++ src/components/bio-main.js
@@ -14,15 +14,20 @@ class Bio extends React.Component {
       <div
         className={styles.container}
       >
+        <div
+          className={styles.imgContainer}  
+        > 
           <img
             src={profilePic}
             alt={`Jayant Tiwari`}
             className={styles.img}
           />
-        <div>
+        </div>
+        <div
+          className={styles.bioContainer}
+        >
           <p className={styles.body}>
-            My name is <strong>Jayant </strong> 
-            Alexa@Amazon
+            SDE at Alexa@Amazon
 
           </p>
           <Link to={`/about/`} className={styles.body}>
---
+++ src/pages/markdown/python_under_the_hood.md
@@ -0,0 +1,12 @@
+---
+title: "Improve performance in Python"
+date: "2019-02-26T00:00:00Z"
+layout: post
+draft: true
+path: "/markdown/python_under_the_hood"
+category: "Category"
+description: "Description"
+tags: ["Engineering", "Draft"]
+---
+
+This article will explore how Python implements commonly used datastructures during run time. Doing so will help identify potential bottleneck in performance.
