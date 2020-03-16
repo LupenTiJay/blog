55757952760e9fd61171336742b348ec3ebb40c5
LupenTiJay <jayant.tiwari777@gmail.com>
2020-03-15 20:53:08 -0700

    remove bio

--- src/pages/about.js
+++ src/pages/about.js
@@ -27,8 +27,7 @@ export default ({ data }) =>
                     A little bit about me
                   </h1>
                   <p className={styles.body}>
-                    I'm currently a 4th year student in Computer Engineering. Heavily involved with technology. <br/>
-                    Playing basketball, making music and writing cool software are my hobbies
+                    SDE at Alexa Internet @ Amazon
                   </p>
                   {/* <div className={styles.body}>
                     <a
